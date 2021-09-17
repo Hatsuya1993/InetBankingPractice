001ebf5ce37da7e287889a56a08fd33eeca606e5
Hazrul <hal1993@hotmail.com>
2021-09-17 22:46:42 +0800

    Updates

--- src/test/java/com/inetBanking/testCases/TC_LoginTest_001.java
+++ src/test/java/com/inetBanking/testCases/TC_LoginTest_001.java
@@ -1,14 +1,22 @@
 package com.inetBanking.testCases;
 
 import com.inetBanking.pageObjects.LoginPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TC_LoginTest_001 extends BaseClass {
 
     @Test
-    public void loginTest(){
+    public void loginTest() {
         driver.get(baseURL);
         LoginPage lp = new LoginPage(driver);
+        lp.setUserName(username);
+        lp.setPassword(password);
+        lp.clickSubmit();
+        if(driver.getTitle().equalsIgnoreCase("Guru99 Bank Manager HomePage")){
+            Assert.assertTrue(true);
+        }
+        else Assert.fail();
     }
 
 }
