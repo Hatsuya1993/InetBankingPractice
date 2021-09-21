c7b4ceb3b751bafbe48a7d0a4f9f79c11d1159db
Hazrul <hal1993@hotmail.com>
2021-09-21 17:32:25 +0800

    New

--- src/test/java/com/inetBanking/testCases/TC_LoginTest_001.java
+++ src/test/java/com/inetBanking/testCases/TC_LoginTest_001.java
@@ -1,13 +1,16 @@
 package com.inetBanking.testCases;
 
 import com.inetBanking.pageObjects.LoginPage;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class TC_LoginTest_001 extends BaseClass {
 
     @Test
-    public void loginTest() {
+    public void loginTest() throws IOException {
         logger.info("URL is opened");
         LoginPage lp = new LoginPage(driver);
         lp.setUserName(username);
@@ -15,11 +18,12 @@ public class TC_LoginTest_001 extends BaseClass {
         lp.setPassword(password);
         logger.info("Password is entered");
         lp.clickSubmit();
-        if(driver.getTitle().equalsIgnoreCase("Guru99 Bank Manager HomePage")){
+        if(driver.getTitle().equalsIgnoreCase("Guru99 Bank Manager HomePag")){
             Assert.assertTrue(true);
             logger.info("Test Pass");
         }
         else {
+            captureScreen(driver, "logintest");
             Assert.fail();
             logger.info("Test Failed");
         }
